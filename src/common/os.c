2956625954435531f1f6f688dc1aa09761611dda
Dagan Gan <daganboy@gmail.com>
2015-09-18 18:45:23 -0400

    Made code compatible with TCC. Had to make getExeType return a pointer(also renamed to getExecType)

--- src/common/os.c
+++ src/common/os.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Executable getExeType(char* filename){
-	//Define exectype
-	Executable exectype;
-	exectype.is_win=0;
-	exectype.is_elf=0;
-	exectype.is_msi=0;
-	exectype.is_dll=0;
-	exectype.is_exec=0;
-	
+struct Executable* getExecType(const char* filename){
+	//Initialize exectype
+	Executable* exectype=(Executable*)malloc(sizeof(*exectype));
+	exectype->is_win=0;
+	exectype->is_elf=0;
+	exectype->is_msi=0;
+	exectype->is_dll=0;
+	exectype->is_managed=0;
+	exectype->is_exec=0;
+
 	//Open binary file
 	FILE* bf=fopen(filename,"rb");
 	if(bf==NULL){
@@ -18,12 +19,12 @@ Executable getExeType(char* filename){
 		return exectype;
 	}
 	
-	//check size
+	//check file size
 	fseek(bf,0,SEEK_END);
 	unsigned int bf_size=ftell(bf);
 	fseek(bf,0,SEEK_SET);
 	if(bf_size==0){printf("Warning - os.c - bf_size==0\n");return exectype;}
-	
+
 	//Read and close file
 	char* binary=(char*)malloc(sizeof(char*)*bf_size);
 	fread(binary,1,bf_size,bf);
@@ -31,31 +32,34 @@ Executable getExeType(char* filename){
 	
 	//Windows
 	if(binary[108]=='D' && binary[109]=='O' && binary[110]=='S'){
-		exectype.is_win=1;
-		exectype.is_exec=1;
+		exectype->is_win=1;
+		exectype->is_exec=1;
 	}
 	
 	//Linux
 	if(binary[0]=='\x7f' && binary[1]=='E' && binary[2]=='L' && binary[3]=='F'){
-		exectype.is_elf=1;
-		exectype.is_exec=1;
+		exectype->is_elf=1;
+		exectype->is_exec=1;
 	}
 	
 	//Free memory
 	free(binary);
 	
 	//Return executable type
-	return exectype;
+	return (exectype);
 }
 
 
-int64_t getFileSize(char* filename){
+int64_t getFileSize(const char* filename){
 	//Open file
 	FILE* fp=fopen(filename,"rb");
-	if(fp==NULL)return -1;
-
+	if(fp==NULL){return -1;}
+	
 	//check size
-	fseek(fp,0L,SEEK_END);
-	return ftell(fp);
+	fseek(fp,0,SEEK_END);
+	int64_t fp_size=ftell(fp);
+	fclose(fp);
+	
+	return fp_size;
 	
 }
--- src/common/os.h
+++ src/common/os.h
@@ -1,27 +1,33 @@
 /*
   Operating system related structs and functions
 */
-#include <stdbool.h>
-#include <stdint.h>
+#ifndef HEADER_OS
+	#define HEADER_OS 1
+	#include <stdbool.h>
+	#include <stdint.h>
 
-//Structure of executable program
-typedef struct Executable {
-	//overall;
-	bool is_exec;
-	
-	//System
-	bool is_win;
-	bool is_elf;
-	
-	//Windows
-	bool is_msi;
-	bool is_dll;
-	
-} Executable;
+	//Structure of executable program
+	typedef struct Executable {
+		//overall;
+		bool is_exec;
+		
+		//System
+		bool is_win;
+		bool is_elf;
+		
+		//Windows
+		bool is_msi;
+		bool is_dll;
+		bool is_managed;//.NET
+		
+		
+	} Executable;
 
 
-//Get executable type
-Executable getExeType(char* filename);
+	//Get executable type
+	struct Executable* getExecType(const char* filename);
 
-//Get file size in bytes (-1 means error)
-int64_t getFileSize(char* filename);
+	//Get file size in bytes (-1 means error)
+	int64_t getFileSize(const char* filename);
+
+#endif
