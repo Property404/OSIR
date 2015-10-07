7b4b3f7a4deeef498cddb9b12179579b8893bd87
Martinez <daganmartinez0448@MAC-P1KAF8J4.local>
2015-10-07 13:50:40 -0400

    Added UNIX code to web.c (compiled on OS X, needs to be tested on Linux)

--- src/common/web.c
+++ src/common/web.c
@@ -1,69 +1,102 @@
 #include "web.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #ifdef _WIN32
 	#include <winsock2.h>
 #else
+	#include <unistd.h>
 	#include <sys/socket.h>
+	#include <netdb.h>
+	#include <netinet/in.h>
 #endif
-#define REPLY_LENGTH 200000
-
+#define SERVER_REPLY_LENGTH 200000
+#define HTTP_PORT_NUMBER 80
 int sendHTTPRequest(char **server_reply,const char* hostname, const char* message){
 	//Allocate memory to prepare server reply
-	*server_reply=(char*)malloc(sizeof(char)*REPLY_LENGTH);
+	*server_reply=(char*)malloc(sizeof(char)*SERVER_REPLY_LENGTH);
+
+	//Unix socket code
+	#ifndef _WIN32
+	int sockfd, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	
+	//Create socket point
+	sockfd=socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd<0){fprintf(stderr,"Error: sendHttpRequest: error opening socket\n");return 0;}
+	
+	server=gethostbyname(hostname);
+	if(server==NULL){fprintf(stderr, "Error: sendHTTPRequest: no such host\n");return 0;}
+
+	bzero((char*) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr,server->h_length);
+	serv_addr.sin_port=htons(HTTP_PORT_NUMBER);
+
+	//Now connect to the server
+	if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0){fprintf(stderr,"Error: sendHTTPRequest: connection error\n");return 0;}
+	
+	//Send message to the server
+	if(write(sockfd, message, strlen(message))<0){fprintf(stderr,"Error: sendHTTPRequest: Can't write to socket\n");return 0;}
+	
+	//Read server resposne
+	if(read(sockfd, *server_reply, SERVER_REPLY_LENGTH)<0){fprintf(stderr,"Error: sendHTTPRequest: Can't read from socket\n");return 0;}
+	#endif
 	
 	//Windows socket code
 	#ifdef _WIN32
 	WSADATA wsa;
-    SOCKET s;
-    struct sockaddr_in server;
-    int recv_size;
+	SOCKET s;
+	struct sockaddr_in server;
+	int recv_size;
  
-    //Initialize Winsock
-    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-    {
-        fprintf(stderr,"Failed. Error Code : %d\n",WSAGetLastError());
-        return 0;
-    }
-     
-     
-    //Create a socket
-    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
-    {
-        fprintf(stderr,"Could not create socket : %d\n" , WSAGetLastError());
-    }
+	//Initialize Winsock
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		fprintf(stderr,"Failed. Error Code : %d\n",WSAGetLastError());
+		return 0;
+	}
+	 
+	 
+	//Create a socket
+	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	{
+		fprintf(stderr,"Could not create socket : %d\n" , WSAGetLastError());
+	}
  
   
-     
+	 
 	//Prepare server struct
-    server.sin_addr.s_addr = inet_addr(hostname);//Should be IP address
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 80 );
+	server.sin_addr.s_addr = inet_addr(hostname);//Should be IP address
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 80 );
  
-    //Connect to remote server
-    if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
+	//Connect to remote server
+	if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
 		fprintf(stderr,"connect error\n");
-        return 0;
-    }
-     
-     
-	//Send message
-    if( send(s , message , strlen(message) , 0) < 0)
-    {
-        fprintf(stderr,"Send failed\n");
-        return 0;
-    }
-     
-    //Receive a reply from the server
-    if((recv_size = recv(s , *server_reply , REPLY_LENGTH , 0)) == SOCKET_ERROR)
-    {
-        fprintf(stderr,"recv failed\n");
 		return 0;
-    }
-     
+	}
+	 
+	 
+	//Send message
+	if( send(s , message , strlen(message) , 0) < 0)
+	{
+		fprintf(stderr,"Send failed\n");
+		return 0;
+	}
+	 
+	//Receive a reply from the server
+	if((recv_size = recv(s , *server_reply , SERVER_REPLY_LENGTH , 0)) == SOCKET_ERROR)
+	{
+		fprintf(stderr,"recv failed\n");
+		return 0;
+	}
+	 
  
-    //Add a NULL terminating character
-    (*server_reply)[recv_size] = '\0';
+	//Add a NULL terminating character
+	(*server_reply)[recv_size] = '\0';
 	
 	#endif
 	return 1;
@@ -127,7 +160,7 @@ char* getHTML(const char* url){
 	
 	//Get HTTP response
 	char* reply;
-	char* html=(char*)malloc(REPLY_LENGTH*sizeof(char));
+	char* html=(char*)malloc(SERVER_REPLY_LENGTH*sizeof(char));
 	if(!sendHTTPRequest(&reply,url_hostname,get_request)){
 		strcpy(html,"!ERROR");
 		
