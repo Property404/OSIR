4009868f5b6a5997d248cd86187fb17ecb83f3b6
Dagan Gan <daganboy@gmail.com>
2015-11-24 12:52:01 -0500

    Emergency commit

--- tests/build.bat
+++ tests/build.bat
@@ -4,11 +4,19 @@ REM set default C compiler
 set c_compiler=tcc
 
 REM build playground files
-mkdir playground
+if not exist playground mkdir playground
 %c_compiler% source\hello.c
-move hello.exe playground\hello.exe
+move /y hello.exe playground\hello.exe > nul
 echo We dont need no education > playground\dummy.txt
 
+REM build system test directories
+if not exist infection_test mkdir infection_test
+cd infection_test
+copy /y ..\playground\hello.exe hello.exe > nul
+copy /y hello.exe+hello.exe hello_concat.exe > nul
+copy /y hello_concat.exe+hello_concat.exe hello_concat2.exe > nul
+copy /y hello_concat2.exe+hello_concat2.exe hello_concat3.exe > nul
+cd ..
 REM build tests
 echo Building client_unit.c
 %c_compiler% source\client_unit.c  ..\client\common\clonelib.c ..\client\common\os.c ..\client\common\weakcrypt.c ..\client\common\thirdparty\b64.c ..\client\common\web.c -lws2_32
